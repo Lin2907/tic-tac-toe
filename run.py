23ddb2c2eb442b3b0461ada2f36cb8e29936d190
Holinda Beshiri <holinda_b@hotmail.com>
2024-04-21 10:34:55 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+# Create a board
+
+board = [ "1" , "2" , "3" ,
+          "4" , "5" , "6",
+          "7" , "8" , "9"]
+
+print(board)
+
+
+
+
+
